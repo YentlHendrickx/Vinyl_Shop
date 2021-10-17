65a910b9e659885abd00f996a2f58d0b172ae20f
Yentl Hendrickx <r0842908@student.thomasmore.be>
2021-10-17 21:04:41 +0200

    Database integration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,55 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('active')->default(true);
+            $table->boolean('admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert(
+            [
+                [
+                  'name' => 'Yentl Hendrickx',
+                  'email' => 'r0842908@student.thomasmore.be',
+                  'admin' => true,
+                    'password' => Hash::make('1234'),
+                    'created_at' => now(),
+                    'email_verified_at' => now()
+                ],
+                [
+                    'name' => 'John Doe',
+                    'email' => 'john.doe@example.com',
+                    'admin' => true,
+                    'password' => Hash::make('admin1234'),
+                    'created_at' => now(),
+                    'email_verified_at' => now()
+                ],
+                [
+                    'name' => 'Jane Doe',
+                    'email' => 'jane.doe@example.com',
+                    'admin' => false,
+                    'password' => Hash::make('user1234'),
+                    'created_at' => now(),
+                    'email_verified_at' => now()
+                ]
+            ]
+        );
+
+        // Add 40 dummy users inside a loop
+        for ($i = 0; $i <= 40; $i++) {
+            DB::table('users')->insert(
+                [
+                    'name' => "ITF User $i",
+                    'email' => "itf_user_$i@mailinator.com",
+                    'password' => Hash::make("itfuser$i"),
+                    'created_at' => now(),
+                    'email_verified_at' => now()
+                ]
+            );
+        }
     }
 
     /**
